8d919ac36cfe9a714c04f4cafc24da46b4f63281
haldre <haldre1@gmail.com>
2017-02-08 16:51:58 -0600

    exploring tidy dataset and made graphs

--- Analysis.R
+++ Analysis.R
@@ -1,9 +1,31 @@
 #Woody Encroachment Analysis - Andrea 
 #started on Jan. 19 2017 
-Andrea_test <- read.csv("~/Haldre/Woody Encroachment/Oakridge-Woody-Encroachment/Andrea_test.csv")
-str (Andrea_test)
-str(SeedlingData_Raw_Compiled)
+#
+library(plyr)
+library(ggplot2)
 
+seedling <- read.csv("SeedlingData_tidy.csv", na.string=c("", "na", "NA"))
+str (seedling)
+
+with(seedling[seedling$Treatment=="LD"|seedling$Treatment=="HD",], table(Treatment, Genus))
+
+seedling$herbexcl<-"NA"
+seedling$herbexcl[seedling$Block==5|seedling$Block==6|seedling$Block==7|seedling$Block==8]<-"excl"
+seedling$herbexcl[seedling$Block==1|seedling$Block==2|seedling$Block==3|seedling$Block==4]<-"open"
+
+with(seedling[seedling$Treatment=="LD"|seedling$Treatment=="HD",], ftable(herbexcl, Treatment, Genus))
+
+sumseedling<-ddply(seedling[seedling$Treatment=="LD"|seedling$Treatment=="HD"& !is.na(seedling$herbexcl),], .(Block, Treatment, herbexcl, Genus), summarize, total=length(Genus))
+
+ggplot(sumseedling[!is.na(sumseedling$Treatment) & !is.na(sumseedling$herbexcl),], aes(Treatment, total))+
+  geom_boxplot()+
+  facet_grid(herbexcl~Genus)+
+  theme_bw()
+
+ggplot(sumseedling[!is.na(sumseedling$Treatment) & !is.na(sumseedling$herbexcl),], aes(herbexcl, total))+
+  geom_boxplot()+
+  facet_grid(Treatment~Genus)+
+  theme_bw()
 
 # Simple Bar Plot 
 counts <- table(mtcars$gear)
