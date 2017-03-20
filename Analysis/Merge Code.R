7a43873810230614332e57e34063fccf9a92a7f5
haldre <haldre1@gmail.com>
2017-03-20 17:15:54 -0500

    Haldre's file organization during meeting

--- Analysis/Merge Code.R
+++
@@ -1,8 +0,0 @@
-#haven't done 
-
-write.csv(SeedlingData_Tidy, "SeedlingData_Tidy.csv")
-SeedlingData_Tidy <- read.csv("SeedlingData_Tidy.csv", na.string=c("", "na", "NA"))
-
-merged.data <- merge(SeedlingData_Tidy, SeedlingGPS, by="ID")
-#can't get it to recognize SeedlingGPS without importing to environment 
-write.csv(merged.data, "Merge.csv")
