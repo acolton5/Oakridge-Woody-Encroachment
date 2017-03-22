5b4cf64952ab7ab60466668135fb75685b23304e
haldre <haldre1@gmail.com>
2017-03-22 12:46:45 -0500

    update with new column names

--- Analysis/Analysis.R
+++ Analysis/Analysis.R
@@ -5,27 +5,30 @@ library(plyr)
 library(ggplot2)
 
 #read in data
-seedling <- read.csv("SeedlingData_tidy.csv", na.string=c("", "na", "NA"))
+seedling <- read.csv("Data/tidy/SeedlingData_tidy.csv", na.string=c("", "na", "NA"))
 str (seedling)
 
 #explore data 
 with(seedling[seedling$Treatment=="LD"|seedling$Treatment=="HD",], table(Treatment, Genus))
 
-seedling$herbexcl<-"NA"
-seedling$herbexcl[seedling$Block==5|seedling$Block==6|seedling$Block==7|seedling$Block==8]<-"excl"
-seedling$herbexcl[seedling$Block==1|seedling$Block==2|seedling$Block==3|seedling$Block==4]<-"open"
+seedling$Herbivory<-"NA"
+seedling$Herbivory[seedling$Block==5|seedling$Block==6|seedling$Block==7|seedling$Block==8]<-"excl"
+seedling$Herbivory[seedling$Block==1|seedling$Block==2|seedling$Block==3|seedling$Block==4]<-"open"
 
-with(seedling[seedling$Treatment=="LD"|seedling$Treatment=="HD",], ftable(herbexcl, Treatment, Genus))
+with(seedling[seedling$Treatment=="LD"|seedling$Treatment=="HD",], ftable(Herbivory, Treatment, Genus))
 
-sumseedling<-ddply(seedling[seedling$Treatment=="LD"|seedling$Treatment=="HD"& !is.na(seedling$herbexcl),], .(Block, Treatment, herbexcl, Genus), summarize, total=length(Genus))
+sumseedling<-ddply(seedling[seedling$Treatment=="LD"|seedling$Treatment=="HD"& !is.na(seedling$Herbivory),], .(Block, Treatment, Herbivory, Genus), summarize, total=length(Genus))
 
-ggplot(sumseedling[!is.na(sumseedling$Treatment) & !is.na(sumseedling$herbexcl),], aes(Treatment, total))+
+ggplot(sumseedling[!is.na(sumseedling$Treatment) & !is.na(sumseedling$Herbivory),], aes(Treatment, total, color=Herbivory))+
+  geom_boxplot()
+
+ggplot(sumseedling[!is.na(sumseedling$Treatment) & !is.na(sumseedling$Herbivory),], aes(Treatment, total))+
   geom_boxplot()+
-  facet_grid(herbexcl~Genus)+
+  facet_grid(Herbivory~.)+
   theme_bw()
 
-ggplot(sumseedling[!is.na(sumseedling$Treatment) & !is.na(sumseedling$herbexcl),], aes(herbexcl, total))+
-  geom_boxplot()+
+ggplot(sumseedling[!is.na(sumseedling$Treatment) & !is.na(sumseedling$Herbivory),], aes(Genus, total, color=Herbivory))+
+  geom_boxplot()
   facet_grid(Treatment~Genus)+
   theme_bw()
 
@@ -37,20 +40,20 @@ str (seedling)
 
 with(Seedling[Seedling$Treatment=="LD"|Seedling$Treatment=="HD",], table(Treatment, Genus))
 
-Seedling$herbexcl<-"NA"
-Seedling$herbexcl[Seedling$Block==5|Seedling$Block==6|Seedling$Block==7|Seedling$Block==8]<-"excl"
-Seedling$herbexcl[Seedling$Block==1|Seedling$Block==2|Seedling$Block==3|Seedling$Block==4]<-"open"
+Seedling$Herbivory<-"NA"
+Seedling$Herbivory[Seedling$Block==5|Seedling$Block==6|Seedling$Block==7|Seedling$Block==8]<-"excl"
+Seedling$Herbivory[Seedling$Block==1|Seedling$Block==2|Seedling$Block==3|Seedling$Block==4]<-"open"
 
-with(Seedling[Seedling$Treatment=="LD"|Seedling$Treatment=="HD",], ftable(herbexcl, Treatment, Genus))
+with(Seedling[Seedling$Treatment=="LD"|Seedling$Treatment=="HD",], ftable(Herbivory, Treatment, Genus))
 
-sumSeedling<-ddply(Seedling[Seedling$Treatment=="LD"|Seedling$Treatment=="HD"& !is.na(Seedling$herbexcl),], .(Block, Treatment, herbexcl, Genus), summarize, total=length(Genus))
+sumSeedling<-ddply(Seedling[Seedling$Treatment=="LD"|Seedling$Treatment=="HD"& !is.na(Seedling$Herbivory),], .(Block, Treatment, Herbivory, Genus), summarize, total=length(Genus))
 
-ggplot(sumSeedling[!is.na(sumSeedling$Treatment) & !is.na(sumSeedling$herbexcl),], aes(Treatment, total))+
+ggplot(sumSeedling[!is.na(sumSeedling$Treatment) & !is.na(sumSeedling$Herbivory),], aes(Treatment, total))+
   geom_boxplot()+
-  facet_grid(herbexcl~Genus)+
+  facet_grid(Herbivory~Genus)+
   theme_bw()
 
-ggplot(sumSeedling[!is.na(sumSeedling$Treatment) & !is.na(sumSeedling$herbexcl),], aes(herbexcl, total))+
+ggplot(sumSeedling[!is.na(sumSeedling$Treatment) & !is.na(sumSeedling$Herbivory),], aes(Herbivory, total))+
   geom_boxplot()+
   facet_grid(Treatment~Genus)+
   theme_bw()
