8a0276488ba1fb2aab7a941aabfd62024cfa9cf2
haldre <haldre1@gmail.com>
2017-03-22 12:46:53 -0500

    haldre edits during meeting

--- Analysis/Height.R
+++ Analysis/Height.R
@@ -2,13 +2,9 @@
 # Analysis of height by diversity trt and herbivore trt ##
 
 #### General notes about next steps ####
-#in munging script, add NA's for all blank cells (read.csv(file, na.strings=c("", "NA", "na")))
-#make decision about LD/edge, LD/HD, 
+#make decision about LD/edge, LD/HD
 
 #in raw data
-#figure out what white mulberry is. 
-#figure out seedling 155 (if no data, then put NA's all across)
-#distinguish between couldn't identify, and NA (meaning not a seedling to be included in study) in genus and species
 
 #how much area is in LD vs HD? (2x as much!)
 
@@ -20,41 +16,39 @@ library(ggplot2)
 library(car)
 
 #load data
-setwd("Data/Tidy")
-Seedling <- read.csv("SeedlingData_Tidy.csv", na.strings=c("", "NA", "na"))
+Seedling <- read.csv("Data/Tidy/SeedlingData_Tidy.csv", na.strings=c("", "NA", "na"))
 
 #explore data
 str(Seedling)
 head(Seedling)
-summary(Seedling)
-Seedling$Height<-as.numeric(Seedling$Height)
+summary(Seedling$height)
 
 ###### Data Exploration ############
 
 ##a.  Outliers in Y / Outliers in X 
 #i.	plot response and predictors to check for outliers  (only with continuous data)
 #1.	Use Mydotplot or dotplot or boxplot, identify outliers
-
-hist(Seedling$Height, ylab = "Heights")
+dotchart(Seedling$height)
+hist(Seedling$height, ylab = "heights")
 #above result in boxplot of heights, no outliers, skewed left(?), most heights short? 
 
-#separating by block, there are outliers in block 1 and 7 
-boxplot(Seedling$Height~Seedling$Block, ylab="Height (m)", xlab= "Block", main="Heights of Woody Stems")
+#separating by block - definite pattern of tallest individuals being in blocks 5-8. Lot more variance in exclosure than open. 
+boxplot(Seedling$height~Seedling$Block, ylab="height (cm)", xlab= "Block", main="heights of Woody Stems")
 
-ggplot(Seedling, aes(Treatment, Height, color=Herbivory))+
+ggplot(Seedling, aes(Herbivory, height, color=Treatment))+
   geom_boxplot() 
-with(Seedling[Seedling$Treatment=="LD"|Seedling$Treatment=="HD",], table(Treatment, Height))
   
-#collinearity x, samples uneven - can't use anova, unequal sample sizes? 507 vs 152
+#Have both diversity trts represented for both herbivory categories
+with(Seedling[Seedling$Treatment=="LD"|Seedling$Treatment=="HD",], table(Treatment, Date))
 with(Seedling, table(Herbivory, Treatment))
 
-# y vs x
-ggplot(Seedling, aes(Treatment, Height..m., color=Herbivory, ylab="Heights (m)", main="Heights by Diversity and Herbivory Treatments")+
+# y vs x - same as before
+ggplot(Seedling, aes(Treatment, height, color=Herbivory, ylab="heights (m)", main="heights by Diversity and Herbivory Treatments"))+
   geom_boxplot()
 
 #are y's independent
-#checks for any variance patterns over date - i don't think i see any? 
-ggplot(Seedling, aes(Treatment, Height..m., color=Herbivory))+
+#checks for any variance patterns over date - no patterns, or really any reason to believe there would be
+ggplot(Seedling, aes(Treatment, height, color=Herbivory))+
   geom_boxplot()+
   facet_grid(.~Date)
 with(Seedling, ftable(Herbivory, Treatment, Date))
@@ -63,11 +57,11 @@ with(Seedling, ftable(Herbivory, Treatment, Date))
 #*****************************ANALYSIS**************************
 
 #ANOVAS I don't think I can use - this is a linear model ie normal dis. which i don't have? 
-webmod1<-lm(Height~Herbivory*Treatment, data=Seedling[Seedling$Treatment=="LD" | Seedling$Treatment =="HD",])
+webmod1<-lm(height~Herbivory*Treatment, data=Seedling[Seedling$Treatment=="LD" | Seedling$Treatment =="HD",])
 summary(webmod1)
 anova(webmod1)
 
-webmod2<-lm(Height~Herbivory+Treatment, data=Seedling[Seedling$Treatment=="LD" | Seedling$Treatment =="HD",])
+webmod2<-lm(height~Herbivory+Treatment, data=Seedling[Seedling$Treatment=="LD" | Seedling$Treatment =="HD",])
 summary(webmod2)
 anova(webmod2)
 confint(webmod2)
@@ -76,11 +70,11 @@ head(model.matrix(webmod1))
 
 
 #linear mixed model
-HeightMod <- lmer(Height..m. ~ Treatment * Herbivory + (1|Block), data=Seedling)
+heightMod <- lmer(height ~ Treatment * Herbivory + (1|Block), data=Seedling)
 #do i need to run this again for date
-summary(HeightMod)
-coef(summary(HeightMod))
-confint(HeightMod)
+summary(heightMod)
+coef(summary(heightMod))
+confint(heightMod)
 
 
 #some extra munge (move to munging code, not analysis)
