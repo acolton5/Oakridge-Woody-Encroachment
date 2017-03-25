2625d64e2f870264bbae64e7ea48dfd82e8f3ae2
haldre <haldre1@gmail.com>
2017-03-25 17:18:51 -0500

    haldre edits - finished analysis & made figure

--- Analysis/Height.R
+++ Analysis/Height.R
@@ -11,81 +11,89 @@
 #make decision about LD/edge, LD/HD
 #how much area is in LD vs HD? (2x as much!) Need to account for this. 
 
- #########################
- #load libraries
-Seedling <- read.csv("Data/Tidy/SeedlingData_Tidy.csv", na.strings=c("", "NA", "
- #explore data
- str(Seedling)
- head(Seedling)
+#########################
+#load libraries
+library(lme4)
+library(ggplot2)
+
+#load data
+Seedling <- read.csv("Data/Tidy/SeedlingData_Tidy.csv", na.strings=c("", "NA", ""))
+
+#explore data
+str(Seedling)
+head(Seedling)
 
 ### fix up dataset ###
-#remove those
+#remove the grapevines and the other few that are missing a height
+Seedling_ht<-Seedling[!is.na(Seedling$height),] #removes 101 observations
 
- ###### Data Exploration ############
+###### Data Exploration ############
  
- ##a.  Outliers in Y / Outliers in X 
- #i.	plot response and predictors to check for outliers  (only with continuous data)
+##a.  Outliers in Y / Outliers in X 
+#i.	plot response and predictors to check for outliers  (only with continuous data)
 #1.	Use dotplot or boxplot, identify outliers
-dotchart(Seedling$height, xlab="Height (cm)", main = "Seedling Heights Again")
- hist(Seedling$height, ylab = "Number of Individuals", xlab= "Height (cm)", main = "Seedling Heights")
+dotchart(Seedling_ht$height, xlab="Height (cm)", main = "Seedling Heights Again") #no obvious outliers
+ hist(Seedling_ht$height, ylab = "Number of Individuals", xlab= "Height (cm)", main = "Seedling Heights")
  #above result in boxplot of heights, no outliers, skewed left(?), most heights short? 
  
 #Explore variance - are variances equal?
  #separating by block - definite pattern of tallest individuals being in blocks 5-8. Lot more variance in exclosure than open. 
-boxplot(Seedling$height~Seedling$Block, ylab="height (cm)", xlab= "Block", main="heights of Woody Stems")
+boxplot(Seedling_ht$height~Seedling_ht$Block, ylab="height (cm)", xlab= "Block", main="heights of Woody Stems")
  
- ggplot(Seedling, aes(Herbivory, height, color=Treatment))+
+ ggplot(Seedling_ht, aes(Herbivory, height, color=Treatment))+
    geom_boxplot() 
    
 #.	Sufficient data?  
 ##As a rule of thumb, (all models), should have 15 to 20 observations for each parameter. So, if have 50 observations, should really only have 3 parameters. 
-nrow(Seedling) #663 rows, three parameters, so we're good. 
+nrow(Seedling_ht) #556 rows, three parameters, so we're good. 
 
 #Check to make sure both diversity trts represented for both herbivory categories
-with(Seedling, table(Herbivory, Treatment))
+with(Seedling_ht, table(Herbivory, Treatment))
  
 #check to see if a lot of NA's in height
-length(Seedling[is.na(Seedling$height),]) # 11 rows where height is NA
+nrow(Seedling_ht[is.na(Seedling_ht$height),]) # 0 rows where height is NA
 
  # y vs x - same as before
- ggplot(Seedling, aes(Treatment, height, color=Herbivory, ylab="heights (m)", main="heights by Diversity and Herbivory Treatments"))+
+ ggplot(Seedling_ht, aes(Treatment, height, color=Herbivory, ylab="heights (m)", main="heights by Diversity and Herbivory Treatments"))+
    geom_boxplot()
  
 #are y's independent?
 #checks for any variance patterns over date - no patterns, or really any reason to believe there would be
- ggplot(Seedling, aes(Treatment, height, color=Herbivory))+
+ ggplot(Seedling_ht, aes(Treatment, height, color=Herbivory))+
    geom_boxplot()+
    facet_grid(.~Date)
 
 #Check to see what the pattern is by date in terms of when collected data
-with(Seedling, ftable(Herbivory, Date)) #did most of hte open's first, then exclosures. But I don't think date should be important for woody stems. 
+with(Seedling_ht, ftable(Herbivory, Date)) #did most of hte open's first, then exclosures. But I don't think date should be important for woody stems. 
 
- with(Seedling, ftable(Herbivory, Treatment, Date))
+ with(Seedling_ht, ftable(Herbivory, Treatment, Date))
  
  
- #*****************************ANALYSIS**************************
+#*****************************ANALYSIS*************************
  
-#linear model - height appears normally distributed
-heightMod <- lmer(height ~ Treatment * Herbivory + (1|Block), data=Seedling)
+#linear model - height appears normally distributed, but residuals look better when height is logged
+heightMod <- lmer(log(height) ~ Treatment * Herbivory + (1|Block), data=Seedling)
 summary(heightMod)
 confint(heightMod)
 
 #the interaction is not significant. Try without the interaction
-heightMod2 <- lmer(log(height) ~ Treatment + Herbivory + (1|Block), data=Seedling)
+heightMod2 <- lmer(log(height) ~ Treatment + Herbivory + (1|Block), data=Seedling_ht)
 summary(heightMod2)
 confint(heightMod2) #shows that seedlings in herbivory open are shorter than those in exclosure
 
 #Model validation
 #A. Look at homogeneity: plot fitted values vs residuals
-#method 1: plot(heightMod)
+#method 1: 
+plot(heightMod2)
 
 #method 2: 
 #extract residuals
 E1 <- resid(heightMod2, type = "pearson")
 
-#plot fitted vs residuals
+#extract fitted values
 F1 <- fitted(heightMod2, type = "response")
 
+#plot fitted vs residuals
 par(mfrow = c(2,2), mar = c(5,5,2,2))
 plot(x = F1, 
     y = E1, 
@@ -100,11 +108,28 @@ abline(h = 0, lty = 2)
 #      plot residuals vs each covariate in the model
 #      plot residuals vs each covariate not in the model
 #      Common sense 
-plot(x=Seedling$Herbivory, y=F1) #heterogeneity in residuals bt Herbivory trts
-plot(x=Seedling$Diversity, y=F1) #heterogeneity in residuals wrt Diversity trts
-plot(x=Seedling$Block, y=F1) #residual variance in random effects
+plot(x=Seedling_ht$Herbivory, y=E1) #heterogeneity in residuals bt Herbivory trts 
+plot(x=Seedling_ht$Treatment, y=E1) #heterogeneity in residuals wrt Diversity trts
+plot(x=Seedling_ht$Block, y=E1) #residual variance in random effects
 
 #D. Look at normality of residuals: histogram
 hist(E1) #look more normal when height is logged than on original scale
 
 #graph it!
+
+sumseedling <- ddply(Seedling_ht, c("Herbivory", "Treatment"), summarise,
+               N    = length(height),
+               mean = mean(height),
+               sd   = sd(height),
+               se   = sd / sqrt(N))
+
+group.colors <- c("HD"="#E69F00", "LD"="#D55E00FF", "LD/HD"="#F0E442") #need to change these
+
+ggplot(sumseedling, aes(Herbivory, mean, color=Treatment))+
+  geom_point(stat="identity", position=position_dodge(width=0.4))+
+  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=0.2, position=position_dodge(width=0.4))+
+  scale_color_manual(name="Diversity Treatment", labels=c("High Diversity", "Low Diversity", "High/Low"), values=group.colors)+
+  ylab("Height (m)")+
+  theme_classic()
+
+ggsave("Graphics/height_figure.png")
