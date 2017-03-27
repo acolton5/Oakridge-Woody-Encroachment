73a71c7180c84aad52deffe131bc3f05da6a1d56
haldre <haldre1@gmail.com>
2017-03-26 22:58:32 -0500

    small edits

--- DataWrangling/Make_dataframes.R
+++ DataWrangling/Make_dataframes.R
@@ -1,4 +1,7 @@
 ### Make some new datasets #### 
+library(plyr)
+
+Seedling <- read.csv("Data/Tidy/SeedlingData_Tidy.csv", na.strings=c("", "NA", ""))
 
 #number of seedlings per spp per block
 sppcomposition<-as.data.frame(with(Seedling, table(Block, GenSp)))
@@ -9,6 +12,4 @@ spprich<-ddply(Seedling, .(Block), summarise, spprich=length(unique(GenSp)))
 write.csv(spprich, "Data/Tidy/spprich.csv")
 
 #guide to common name and scientific name
-Seedling<-read.csv("Data/Tidy/SeedlingData_Tidy.csv")
-library(plyr)
 SppID_guide<-ddply(Seedling, .(Common.Name, GenSp), summarise, num=length(GenSp) )
