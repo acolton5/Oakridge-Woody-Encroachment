83f3eee2bfe105c6497a8496550810cfe3b01711
haldre <haldre1@gmail.com>
2017-03-26 22:57:59 -0500

    add new graphics

--- DataWrangling/AdultGPS Munge.R
+++ DataWrangling/AdultGPS Munge.R
@@ -11,23 +11,74 @@ adultgps$common.name<- gsub("\\d", "", adultgps$common.name)
 adultgps$common.name<-trimws(adultgps$common.name)
 
 levels(as.factor(adultgps$common.name))
-#then, make a column for genus, and fill in. 
+#then, make a column for genus and one for spp, and fill in. 
 adultgps$Genus<-"NA"
+adultgps$Species<-"NA"
 
-adultgps$Genus[adultgps$Common.Name== "black cherry"]<- "Prunus"
-adultgps$Species[adultgps$Common.Name== "black cherry"]<- "serotina"
-adultgps$Genus[adultgps$Common.Name== "black walnut"]<- "Prunus"
-adultgps$Species[adultgps$Common.Name== "black cherry"]<- "serotina"
-adultgps$Genus[adultgps$Common.Name== "boxelder"]<- "Prunus"
-adultgps$Species[adultgps$Common.Name== "black cherry"]<- "serotina"
-adultgps$Genus[adultgps$Common.Name== "buckthorn"]<- "Prunus"
-adultgps$Species[adultgps$Common.Name== "black cherry"]<- "serotina"
-adultgps$Genus[adultgps$Common.Name== "buckthorn"]<- "Prunus"
-adultgps$Species[adultgps$Common.Name== "black cherry"]<- "serotina"
-adultgps$Genus[adultgps$Common.Name== "buckthorn"]<- "Prunus"
-adultgps$Species[adultgps$Common.Name== "black cherry"]<- "serotina"
-adultgps$Genus[adultgps$Common.Name== "buckthorn"]<- "Prunus"
-adultgps$Species[adultgps$Common.Name== "black cherry"]<- "serotina"
+adultgps$Genus[adultgps$common.name== "black cherry"]<- "Prunus"
+adultgps$Species[adultgps$common.name== "black cherry"]<- "serotina"
 
-#then, make a column for species, and fill in. 
+adultgps$Genus[adultgps$common.name== "black walnut"]<- "Juglans"
+adultgps$Species[adultgps$common.name== "black walnut"]<- "nigra"
+
+adultgps$Genus[adultgps$common.name== "boxelder"]<- "Acer"
+adultgps$Species[adultgps$common.name== "boxelder"]<- "negundo"
+
+adultgps$Genus[adultgps$common.name== "buckthorn"]<- "Rhamnus"
+adultgps$Species[adultgps$common.name== "buckthorn"]<- "unknown"
+
+adultgps$Genus[adultgps$common.name== "grape"]<- "Vitus"
+adultgps$Species[adultgps$common.name== "grape"]<- "riparia"
+
+adultgps$Genus[adultgps$common.name== "green ash"]<- "Fraxinus"
+adultgps$Species[adultgps$common.name== "green ash"]<- "pennsylvanica"
+
+adultgps$Genus[adultgps$common.name== "hackberry"]<- "Celtis"
+adultgps$Species[adultgps$common.name== "hackberry"]<- "occidentalis"
+
+adultgps$Genus[adultgps$common.name== "honeysuckle"]<- "Lonicera"
+adultgps$Species[adultgps$common.name== "honeysuckle"]<- "unknown"
+
+adultgps$Genus[adultgps$common.name== "mulberry"]<- "Morus"
+adultgps$Species[adultgps$common.name== "mulberry"]<- "alba"
+
+adultgps$Genus[adultgps$common.name== "oak"]<- "Quercus"
+adultgps$Species[adultgps$common.name== "oak"]<- "unknown"
+
+adultgps$Genus[adultgps$common.name== "other vine species"]<- "unknown"
+adultgps$Species[adultgps$common.name== "other vine species"]<- "unknown"
+
+adultgps$Genus[adultgps$common.name== "pear"]<- "Pyrus"
+adultgps$Species[adultgps$common.name== "pear"]<- "calleryana"
+
+adultgps$Genus[adultgps$common.name== "plum"]<- "Prunus"
+adultgps$Species[adultgps$common.name== "plum"]<- "unknown"
+
+adultgps$Genus[adultgps$common.name== "redbud"]<- "Cercis"
+adultgps$Species[adultgps$common.name== "redbud"]<- "canadensis"
+
+adultgps$Genus[adultgps$common.name== "serviceberry"]<- "Amelanchier"
+adultgps$Species[adultgps$common.name== "serviceberry"]<- "unknown"
+
+adultgps$Genus[adultgps$common.name== "si.elm"]<- "Ulmus"
+adultgps$Species[adultgps$common.name== "si.elm"]<- "pumila"
+
+adultgps$Genus[adultgps$common.name== "sil. maple"]<- "Acer"
+adultgps$Species[adultgps$common.name== "sil. maple"]<- "saccharinum"
+
+adultgps$Genus[adultgps$common.name== "sl.elm"]<- "Ulmus"
+adultgps$Species[adultgps$common.name== "sl.elm"]<- "unknown"
+
+adultgps$Genus[adultgps$common.name== "sw.oak"]<- "Quercus"
+adultgps$Species[adultgps$common.name== "sw.oak"]<- "unknown"
+
+adultgps$Genus[adultgps$common.name== "sycamore"]<- "Platanus"
+adultgps$Species[adultgps$common.name== "sycamore"]<- "occidentalis"
+
+adultgps$Genus[adultgps$common.name== "tree with light bark and prombud"]<- "unknown"
+adultgps$Species[adultgps$common.name== "tree with light bark and prombud"]<- "unknown"
+
 #then, make a column for genus + species.
+adultgps$GenSp<-paste(adultgps$Genus,adultgps$Species)
+
+write.csv(adultgps, "Data/Tidy/adultgps_tidy.csv")
