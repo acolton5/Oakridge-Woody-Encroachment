9b43d4e04f0e950b1007939839d7e56f91591eeb
haldre <haldre1@gmail.com>
2017-03-26 22:58:08 -0500

    update merging code

--- DataWrangling/Merge Code.R
+++ DataWrangling/Merge Code.R
@@ -1,10 +1,11 @@
 ### Merge Seedling data with Seedling GPS data ###
 ## Point ID is the column of interest to merge. 
 
-SeedlingData_Tidy <- read.csv("SeedlingData_Tidy.csv", na.string=c("", "na", "NA"))
+SeedlingData_Tidy <- read.csv("Data/Tidy/SeedlingData_Tidy.csv", na.string=c("", "na", "NA"))
+SeedlingData_Tidy$ID<-as.numeric(SeedlingData_Tidy$ID)
+
 seedgps<-read.csv("Data/Tidy/SeedlingGPS.csv")
 
-merged.data <- merge(SeedlingData_Tidy, SeedlingGPS, by="ID")
-#can't get it to recognize SeedlingGPS without importing to environment 
+seedling_gps <- left_join(SeedlingData_Tidy, seedgps, by=c("ID"= "Point_ID"))
 
-write.csv(merged.data, "seedlinggps_tidy.csv")
+write.csv(seedling_gps, "Data/Tidy/seedlinggps_tidy.csv")
